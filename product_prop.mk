97a3a7b508044898299439e6c5eb761735208a5e
CannedShroud <hypersync112@gmail.com>
2020-12-26 00:53:24 +0530

    X2: Set DPI to 460

--- product_prop.mk
+++ product_prop.mk
@@ -27,7 +27,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Override Display Density
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.sf.lcd_density=440
+    ro.sf.lcd_density=460
 
 # Use Vulkan backend for Skia UI rendering
 PRODUCT_PRODUCT_PROPERTIES += \
