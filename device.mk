53647bdf404f8b607035117e65889a02c5195eac
CannedShroud <hypersync112@gmail.com>
2020-12-28 08:53:30 +0530

    X2: Bring back Realme Parts
    This reverts commit 281974efe6d8ed843fe676b8ba18be0c9d900bef.

--- device.mk
+++ device.mk
@@ -77,6 +77,7 @@ PRODUCT_PACKAGES += \
 # Common init scripts
 PRODUCT_PACKAGES += \
     init.qcom.rc \
+    init.devicesetting.rc
 
 PRODUCT_PACKAGES += \
     fstab.qcom
@@ -90,6 +91,7 @@ PRODUCT_PACKAGES += \
 
 # Doze
 PRODUCT_PACKAGES += \
+    devicesettings \
     RealmeDoze \
     RealmeProximityHelper
 
